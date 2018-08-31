c278117d977c407f431d86115bde94a20fc80442
Alex Saltzman <a.saltzman920@gmail.com>
2018-08-31 13:20:54 -0500

    fix filenotfound test

--- gpgrouper/tests/test.py
+++ gpgrouper/tests/test.py
@@ -510,7 +510,8 @@ class TestFull(unittest.TestCase):
                                            '--configfile', CONFIG_FILE,
                                            ])
         # output = pd.read_table(os.path.join(INPUT_DIR, '1_1_1_none_0_e2g.tab'))
-        output = pd.read_table(os.path.join(INPUT_DIR, '1_1_1_none_psms.tab'))
+        # self.assertTrue(1==0, msg='{}'.format('\n'.join(os.listdir('./testdata/two_uniques'))))
+        output = pd.read_table(os.path.join(INPUT_DIR, '1_1_1_none_0_psms.tab'))
         # print(output.columns)
         for col in data_cols:
             self.assertTrue(col in output.columns, msg='{} not found in data file'.format(col))
