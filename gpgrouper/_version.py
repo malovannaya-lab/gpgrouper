626670db8f332d87d7b7d7cb2d6ffb527d114ccc
Alex Saltzman <a.saltzman920@gmail.com>
2018-08-31 12:55:06 -0500

    fix pygrouper-gpgrouper name

--- gpgrouper/_version.py
+++ gpgrouper/_version.py
@@ -6,6 +6,6 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 def _calculate_latest_edit(files, path=BASE_DIR):
     return max([os.stat(os.path.join(path, f)).st_mtime for f in files])
 
-_package_files = ['pygrouper.py', 'subfuncts.py', '_version.py', 'containers.py', 'auto_grouper.py']
+_package_files = ['gpgrouper.py', 'subfuncts.py', '_version.py', 'containers.py', 'auto_grouper.py']
 __version__ = '0.1.040'
 __copyright__ = date.fromtimestamp(_calculate_latest_edit(_package_files)).__str__()
